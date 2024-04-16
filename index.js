441363ffb122e701262c7612029101633ca20cc4
kabilMurugesan <98404076+kabilMurugesan@users.noreply.github.com>
2024-04-16 11:22:35 +0530

    intial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const mysql = require("mysql2")
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
