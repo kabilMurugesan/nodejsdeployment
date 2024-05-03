508e62e152a47e51db3e811f52a6b83f7df8598b
kabilMurugesan <98404076+kabilMurugesan@users.noreply.github.com>
2024-05-03 18:36:55 +0530

    deployment changes

---
+++ scripts/after_install.sh
@@ -0,0 +1,4 @@
+mkdir /home/ubuntu/nodejsdeployment
+cd /home/ubuntu/nodejsdeployment
+
+sudo npm install
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,3 +1,6 @@
 # !/bin/bash
+
+sudo apt-get update
+sudo npm install nodemon -g
 sudo pm2 delete all
 sudo pm2 flush               
