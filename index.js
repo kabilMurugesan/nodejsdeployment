58798051ab56674a28de846be46edefd0957d849
kabilMurugesan <kabil.murugesan@concertidc.com>
2025-09-17 20:39:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -127,7 +127,7 @@ async function startServer() {
         pool = await sql.connect(config);
         console.log('Database is connected');
 
-        const PORT = 4000;
+        const PORT = process.env.PORT || 4000;
         app.listen(PORT, () => {
             console.log(`Server is running on port ${PORT}`);
         });
